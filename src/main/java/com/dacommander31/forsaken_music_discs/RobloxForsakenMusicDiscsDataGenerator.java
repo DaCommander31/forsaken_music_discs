09b03f3758f30c4c6aaed1faa8221ec85133c36e
DaCommander31 <millerjr808@gmail.com>
2025-07-14 13:07:00 +1000

    Initial Commit

---
+++ src/main/java/com/dacommander31/forsaken_music_discs/RobloxForsakenMusicDiscsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dacommander31.forsaken_music_discs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RobloxForsakenMusicDiscsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
